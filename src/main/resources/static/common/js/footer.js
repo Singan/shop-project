855345fa72a3a91c349f8dcf6727bfe69c1705e1
석성희 <sunghee119z@naver.com>
2022-11-01 01:27:26 +0900

    asdasd
    asdasd

---
+++ src/main/resources/static/common/js/footer.js
@@ -0,0 +1,16 @@
+let footer = $("#footer");
+console.log("푸터")
+$(()=>{
+
+})
+footer.html("" +
+    "<div class=\"col-10\">" +
+    "        <p class=\"col-3\">사업자등록번호 : 123-456-789</p>" +
+    "        <p class=\"col-3\">통신판매입신고번호 : 312-2165-1235</p>" +
+    "        <p class=\"col-3\">대표이사 : 나다</p>" +
+    "        <p class=\"col-3\"><a href=\"#\">사업자등록확인</a></p>" +
+    "        <p class=\"col-3\">이메일 : asdasd@google.com</p>" +
+    "        <p class=\"col-3\">주소 : 경기도 성남시 중원구 239-23</p>" +
+    "        <p class=\"col-3\">대표전화 : 123-4151</p>" +
+    "        <p class=\"col-3\"><a href=\"#\">1:1문의 바로가기</a></p>" +
+    "    </div>")
