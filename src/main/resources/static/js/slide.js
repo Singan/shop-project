238907c00bbe51e3df3870574ec7907c402c3c4d
HajongChan2 <gktjswn92@naver.com>
2022-11-01 23:34:52 +0900

    id_pwd html 생성 css 추가, slide js 수정

---
+++ src/main/resources/static/js/slide.js
@@ -0,0 +1,12 @@
+$(function() {
+		var swiper = new Swiper('.swiper-container', {
+			pagination: '.swiper-pagination',
+			nextButton: '.swiper-button-next',
+			prevButton: '.swiper-button-prev',
+			autoplay: 4000 ,
+			slidesPerView: 1,
+			paginationClickable: true,
+			loop: true
+		});
+	});
+	
