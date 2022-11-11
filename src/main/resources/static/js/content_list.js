5ee6714ff24231f65215264fa2e57b91dae95fb5
HajongChan2 <gktjswn92@naver.com>
2022-11-11 18:19:16 +0900

    js css

--- src/main/resources/static/js/content_list.js
+++ src/main/resources/static/js/content_list.js
@@ -1,11 +1,12 @@
+const Time = 400;
 $(document).ready( function() {
 		$("#wishlist").click( function() {
   			if($("#wishlist_content").css("display") == "block"){ 
-				$("#wishlist_content").hide(300);
+				$("#wishlist_content").slideUp(Time);
 				$("#wishlist_dropdown").css({'transform':'rotate(0deg)'});
 			}else if($("#wishlist_content").css("display") == "none"){
-				$("#wishlist_content").show(300);
-				$("#basketlist_content").hide(300);
+				$("#wishlist_content").slideDown(Time);
+				$("#basketlist_content").slideUp(Time);
 				$("#basket_dropdown").css({'transform':'rotate(0deg)'});
 				$("#wishlist_dropdown").css({'transform':'rotate(180deg)'});
 			}
@@ -13,11 +14,11 @@ $(document).ready( function() {
 
 		$("#basketlist").click( function() {
   			if($("#basketlist_content").css("display") == "block"){ 
-				$("#basketlist_content").hide(300);
+				$("#basketlist_content").slideUp(Time);
 				$("#basket_dropdown").css({'transform':'rotate(0deg)'});
 			}else if($("#basketlist_content").css("display") == "none"){
-				$("#basketlist_content").show(300);
-				$("#wishlist_content").hide(300);
+				$("#basketlist_content").slideDown(Time);
+				$("#wishlist_content").slideUp(Time);
 				$("#wishlist_dropdown").css({'transform':'rotate(0deg)'});
 				$("#basket_dropdown").css({'transform':'rotate(180deg)'});
 			}
