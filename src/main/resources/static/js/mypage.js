87c6d3dbad33b1055f5498cf00f5bb38aafffdd7
HajongChan2 <gktjswn92@naver.com>
2022-11-16 23:08:33 +0900

    mypage html 수정 css js mylist.html 생성

---
+++ src/main/resources/static/js/mypage.js
@@ -0,0 +1,8 @@
+$(function(){ 
+    $("#coupon").click(function(){
+        $(".modal").fadeIn();
+    });        
+    $(".modal_out_btn").click(function(){
+        $(".modal").fadeOut();
+    });        
+});
