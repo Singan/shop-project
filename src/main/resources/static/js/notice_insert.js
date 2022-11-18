7e47a7fc977fd2e1379ad530555ce544bfa5c047
HajongChan2 <gktjswn92@naver.com>
2022-11-18 19:29:37 +0900

    html css

---
+++ src/main/resources/static/js/notice_insert.js
@@ -0,0 +1,13 @@
+function readURL(obj) {
+
+    let reader = new FileReader();
+    if(!obj.files.length) {
+        return;
+    }
+    reader.readAsDataURL(obj.files[0]);
+    reader.onload = function (e) {
+        let img = $('<img />');
+        $(img).attr('src', e.target.result);
+        $('#notice_insert_contnet_content').append(img);
+    }
+}
