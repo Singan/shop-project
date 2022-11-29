8aec2f924f05176eca5da18baec9e47499a76af8
HajongChan2 <gktjswn92@naver.com>
2022-11-30 00:05:12 +0900

    js

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,15 +1,3 @@
-$(function() {
-		var swiper = new Swiper('.swiper-container', {
-			pagination: '.swiper-pagination',
-			nextButton: '.swiper-button-next',
-			prevButton: '.swiper-button-prev',
-			autoplay: 4000 ,
-			slidesPerView: 1,
-			paginationClickable: true,
-			loop: true
-		});
-	});
-
 /*
 $(document).ready(function(){
 	$(function() {
--- src/main/resources/static/js/product_order.js
+++ src/main/resources/static/js/product_order.js
@@ -41,4 +41,17 @@ $(document).ready(function() {
 
         }
     });
+
+    $('#coupon').click(function() {
+        $('.modal').fadeIn();
+        $('.modal_content').css({
+            "top": (($(window).height() - $(".modal_content").outerHeight()) / 2 + $(window).scrollTop()) + "px"
+        }).show();
+
+        $("body").css("overflow","hidden");
+    });
+    $(".modal_out_btn").click(function(){
+            $(".modal").fadeOut();
+            $("body").css("overflow","auto");
+    });
 });
