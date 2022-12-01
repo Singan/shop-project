61fe51ff722167643468107651d333a729e0e0ed
HajongChan2 <gktjswn92@naver.com>
2022-12-01 10:06:55 +0900

    htnl

--- src/main/resources/static/js/product_detail.js
+++ src/main/resources/static/js/product_detail.js
@@ -1,61 +1,9 @@
-let count = 1;
-function reviewImage(obj) {
-
-    let reader = new FileReader();
-    if(!obj.files.length) {
-        return;
-    }
-    reader.readAsDataURL(obj.files[0]);
-    reader.onload = function (e) {
-        let img = $('<img />');
-        $(img).attr('src', e.target.result);
-        $('.view_image').append(img);
-        $(img).addClass('appndImage');
-        $(img).attr('id','appndImage' + count);
-        count += 1;
-    }
-}
-
-
-$(document).ready(function () {
-	$(".view_image").click(function (e) {
-		console.log(event.target.id);
-		$("#" + event.target.id).remove();
-		count -= 1;
-	})
-});
-
-
-$(document).ready(function() {
-    let count = $(".number").val();
-    let num = parseInt(count, 10);
-    let plus = $(".plus");
-    let minus = $(".minus");
-    $(".number").on("propertychange change paste input", function(){
-        num = parseInt($(".number").val(), 10);
-    });
-    
-
-    plus.click(function(){
-        num += 1; 
-        $(".number").val(num);
-    });
-    minus.click(function(){
-       if(num > 1){
-            num -= 1;
-            $(".number").val(num);
-       }else{
-            num = 1;
-            return false;
-
-       }
-    });
-        
-});
-
-
 $(window).on('load',function(){
     let price = $(".price").text();
     let text = "원";
     $(".price").text(price + text);
+});
+
+$(window).on('load', function(){
+
 });
