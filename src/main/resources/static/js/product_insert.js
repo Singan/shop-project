ecaa29506b9d84ab4c46656683bfcc430c5764d7
HajongChan2 <gktjswn92@naver.com>
2022-12-05 01:03:02 +0900

    js

--- src/main/resources/static/js/product_insert.js
+++ src/main/resources/static/js/product_insert.js
@@ -16,7 +16,30 @@ function productcontent(obj) {
     }
     reader.readAsDataURL(obj.files[0]);
     reader.onload = function (e) {
-        $('.content_img').attr('src', e.target.result); 
-        a = e.target.result;
+        let img = $('<img />');
+        $(img).attr('src', e.target.result);
+        $('.product_insert_contents').append(img);
     }
 }
+
+$(document).ready(function(){
+    let formData = new formData($("#product_form"));
+
+    formData.append("product_image", $("#product_content_img")[0].files[0]);
+    formData.append("product_content", $(".product_insert_contents").html());
+
+    $('#submit').click(function(){
+        $.ajax({
+            type : "POST",
+            url : "/product/insert",
+            data : formData,
+            processData: false,
+            contentType: false,
+            success : function(data){
+
+            }
+
+        });
+    });
+});
+
