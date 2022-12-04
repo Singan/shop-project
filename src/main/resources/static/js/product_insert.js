e02c6f3a455d87aa71e756e9446dc16b36c6186d
HajongChan2 <gktjswn92@naver.com>
2022-12-05 02:01:29 +0900

    hs

--- src/main/resources/static/js/product_insert.js
+++ src/main/resources/static/js/product_insert.js
@@ -23,27 +23,28 @@ function productcontent(obj) {
 }
 
 $(document).ready(function(){
-    let formData;
-    formData = new formData($("#product_form"));
-    let name = $('input[name=name]');
-    let shortContent = $('input[name=shortContent]');
-    let price = $('input[name=price]');
-    let count = $('input[name=count]');
-    let discountRate = $('input[name=discountRate]');
-    let category = $('select[name=category]');
-    let categorydetail = $('select[name=categorydetail]');
 
-    formData.append("product_name",name.val());
-    formData.append("product_count",shortContent.val());
-    formData.append("product_price",price.val());
-    formData.append("product_count",count.val());
-    formData.append("product_discount",discountRate.val());
-    formData.append("product_category",category.val());
-    formData.append("product_categorydetail",categorydetail.val());
-    formData.append("product_image", $("#product_content_img")[0].files[0]);
-    formData.append("product_content", $(".product_insert_contents").html());
 
     $('#submit').click(function(){
+        let formData;
+        formData = new formData($("#product_form"));
+        let name = $('input[name=name]');
+        let shortContent = $('input[name=shortContent]');
+        let price = $('input[name=price]');
+        let count = $('input[name=count]');
+        let discountRate = $('input[name=discountRate]');
+        let category = $('select[name=category]');
+        let categorydetail = $('select[name=categorydetail]');
+
+        formData.append("name",name.val());
+        formData.append("shortContent",shortContent.val());
+        formData.append("price",price.val());
+        formData.append("count",count.val());
+        formData.append("discountRate",discountRate.val());
+        formData.append("category",category.val());
+        formData.append("categorydetail",categorydetail.val());
+        formData.append("image", $("#product_content_img")[0].files[0]);
+        formData.append("content", $(".product_insert_contents").html());
         $.ajax({
             type : "POST",
             url : "/product/insert",
@@ -52,6 +53,7 @@ $(document).ready(function(){
             contentType: false,
             success : function(data){
                 console.log(data);
+                return false;
             }
 
         });
