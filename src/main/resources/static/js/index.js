ec384fc2769ff3c6148f47620edd223112b78e48
HajongChan2 <gktjswn92@naver.com>
2022-12-04 12:19:09 +0900

    html

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,37 +1,3 @@
-
-$(document).ready(function(){
-	$(function() {
-
-	    var didScroll = false;
-			
-	    $(window).on('scroll', function() { didScroll = true; });
-	    
-	    setInterval(function() {
-	        var bottomHeight = $(window).scrollTop() + $(window).height() + 600;
-	        var docHeight = $(document).height();
-
-	        if (didScroll) {
-	            didScroll = false;
-	            if (bottomHeight > docHeight) infiniteScrollContent();
-	        }
-	    }, 250);
-	    
-	    function infiniteScrollContent() {
-	      
-	        var scrollcon = '';
-	        for (var i = 0; i < 12; i++) {
-	            scrollcon += 
-	            	`<div class="col-12">
-	            		<p>asdasdasd</p>
-	            		<p>asdasdasd</p>
-	            	</div>`;
-	        }			
-	        $("#content_main").append(scrollcon);
-	    }
-	});	
-
-});
-
 $( document ).ready( function() {
     $( window ).scroll( function() {
         if ( $( this ).scrollTop() > 200 ) {
